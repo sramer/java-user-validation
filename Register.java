058f1884b022cafcbbbe9cc996d5c695012ddbfa
sramer <46699764+sramer@users.noreply.github.com>
2019-08-25 20:19:12 +0530

    Update Register.java

--- Register.java
+++ Register.java
@@ -14,13 +14,6 @@ public class Register extends JFrame implements ActionListener {
 	JLabel passwordLabel=new JLabel("PASSWORD");
     JLabel confirmPasswordLabel=new JLabel("CONFIRM PASSWORD");
     JLabel emailLabel=new JLabel("EMAIL");
-    JLabel captchaLabel = new JLabel();
-    JLabel captchaName = new JLabel("What is inside the image");
-    String[] images1 = new String[]{"cat.jpg", "car.jpg", "earpod.jpg", "star.jpg","apple.jpg"};
-    String[] images2 = new String[]{"1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"};
-    String[] images3 = new String[]{"1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"};
-    String[] answers = new String[] {"cat","car","earpod","star","apple"};
-   
 	
 	JTextField nameTextField=new JTextField();
     JComboBox genderComboBox=new JComboBox(gender);
@@ -29,25 +22,8 @@ public class Register extends JFrame implements ActionListener {
     JTextField emailTextField=new JTextField();
     JButton registerButton=new JButton("REGISTER");
     JButton resetButton=new JButton("RESET");
-    JTextField captchaAnswer = new JTextField();
  
 
-	
-	JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,sample,starB;
-	JCheckBox c1,c2,c3,c4,c5,c6,c7,c8,c9;
-	Icon star;
-	Icon pod1,b8Clicked;
-	Icon pod2,b9Clicked;
-	int pod1Captch,pod2Captch,captures;
-	
-    int index1 = (int) (Math.random() * (images1.length - 1));
-    int index2 = (int) (Math.random() * (images2.length - 1));
-    int index3 = (int) (Math.random() * (images3.length - 1));
-    
-	
-	Icon ic1 = new ImageIcon(images1[index1]);
-	Icon ic2 = new ImageIcon(images2[index2]);
-	Icon ic3 = new ImageIcon(images3[index3]);
 	
 	
 	Register() 
@@ -67,24 +43,12 @@ public class Register extends JFrame implements ActionListener {
         emailTextField.setBounds(250,243,165,23);
         registerButton.setBounds(100,600,100,35);
         resetButton.setBounds(300,600,100,35);  
-        captchaAnswer.setBounds(150,500,200,25);
+     	
 		
-		
-		
-		JLabel l1=new JLabel("Verify Captcha");  
-		l1.setBounds(50,30,150,30);  
-		l1.setFont(new Font("Roboto", Font.PLAIN, 20));
-		JLabel l3=new JLabel();  
-		l3.setBounds(380,320,200,20);  
-		l3.setForeground(Color.red);  
-		
-		captchaLabel.setIcon(ic1);
-		captchaLabel.setBounds(150,300,200,200);  
-		captchaName.setBounds(170, 250, 200, 70);
 		
 
 		
-		  add(nameLabel);
+	     add(nameLabel);
 	      add(genderLabel);
 	      add(passwordLabel);
 	      add(confirmPasswordLabel);
@@ -96,9 +60,7 @@ public class Register extends JFrame implements ActionListener {
 	      add(emailTextField);
 	      add(registerButton);
 	      add(resetButton);
-	      add(captchaLabel);
-	      add(captchaAnswer);
-	      add(captchaName);
+	    
 		
 		
 	      
@@ -115,19 +77,6 @@ public class Register extends JFrame implements ActionListener {
 public void actionPerformed(ActionEvent e){
 		
 		
-        if(nameTextField == null) {
-			
-			registerButton.setEnabled(false);
-		}
-		if(passwordField == null) {
-			registerButton.setEnabled(false);
-		}
-		if(confirmPasswordField == null) {
-			registerButton.setEnabled(false);
-		}
-		if(emailTextField == null) {
-			registerButton.setEnabled(false);
-		}
 		
 		
 		if(e.getSource()==registerButton) 
@@ -147,55 +96,8 @@ public void actionPerformed(ActionEvent e){
               
               if(passwordField.getText().equals(confirmPasswordField.getText()))
               {
-            	  if(captchaLabel.getIcon().toString().equals(images1[0]))  {
-            		  
-            		  captchaName.setText("What is this animal");
-                      
-                	  if(captchaAnswer.getText().toString().equals(answers[0])){
-                		  Pstatement.executeUpdate();
-                          JOptionPane.showMessageDialog(null,"Data Registered Successfully");
-                	  }
-                  }
-            	  
-                       if(captchaLabel.getIcon().toString().equals(images1[1]))  {
-      
-                      
-                	  if(captchaAnswer.getText().toString().equals(answers[1])){
-                		  Pstatement.executeUpdate();
-                          JOptionPane.showMessageDialog(null,"Data Registered Successfully");
-                	  }
-                  }
-                       
-                       if(captchaLabel.getIcon().toString().equals(images1[2]))  {
-                    	      
-                           
-                     	  if(captchaAnswer.getText().toString().equals(answers[2])){
-                     		  Pstatement.executeUpdate();
-                               JOptionPane.showMessageDialog(null,"Data Registered Successfully");
-                     	  }
-                       }
-                       
-                       if(captchaLabel.getIcon().toString().equals(images1[3]))  {
-                    	      
-                           
-                     	  if(captchaAnswer.getText().toString().equals(answers[3])){
-                     		  Pstatement.executeUpdate();
-                               JOptionPane.showMessageDialog(null,"Data Registered Successfully");
-                     	  }
-                       }
-                       
-                       if(captchaLabel.getIcon().toString().equals(images1[4]))  {
-                    	      
-                           
-                     	  if(captchaAnswer.getText().toString().equals(answers[4])){
-                     		  Pstatement.executeUpdate();
-                               JOptionPane.showMessageDialog(null,"Data Registered Successfully");
-                     	  }
-                       }
-               
-                  else {
-                	  JOptionPane.showMessageDialog(null,"please verify captcha succesfully");
-                  }
+		   Pstatement.executeUpdate();
+                   JOptionPane.showMessageDialog(null,"Data Registered Successfully");
                   
               }
               else
